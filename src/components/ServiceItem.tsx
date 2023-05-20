bd91ea2c1ea3f1a3ea7046c08d52ff8010b88e94
RaminMajidi <raminmajididev01@gmail.com>
2023-05-20 23:09:45 +0430

    ###

--- src/components/ServiceItem.tsx
+++ src/components/ServiceItem.tsx
@@ -11,7 +11,7 @@ const ServiceItem:React.FC<ServesItemProps> = ({icon,title,text}) =>{
     return(
         <div className="d-flex flex-column align-items-center bg-light rounded-2 text-black  col-5 col-lg-2 m-1" >
             <i className={`bi ${icon} text-warning fs-1`}></i>
-            <h6 className="fw-bold">{title}</h6>
+            <h6 className="fw-bold text-center">{title}</h6>
             <p className="d-none d-md-block text-center">{text}</p>
         </div>
     )
